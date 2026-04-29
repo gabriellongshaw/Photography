fdfbff966aa273f9db9fe02705b1e3039635324a
gabriellongshaw <gabriel.longshaw@gmail.com>
2026-04-29 21:09:52 +0100

    fix (assets/js/data/pages.js): home page url

--- assets/js/data/pages.js
+++ assets/js/data/pages.js
@@ -1,6 +1,9 @@
+const pathArray = window.location.pathname.split('/');
+const base = pathArray.slice(0, 3).join('/') + '/'; 
+
 export const pages = [
-  { name: 'Home', href: './' },
-  { name: 'Aperture', href: 'aperture/' },
-  { name: 'Shutter Speed', href: 'shutter-speed/' },
-  { name: 'ISO', href: 'iso/' },
+  { name: 'Home', href: base },
+  { name: 'Aperture', href: `${base}aperture/` },
+  { name: 'Shutter Speed', href: `${base}shutter-speed/` },
+  { name: 'ISO', href: `${base}iso/` },
 ];
