9ce5c16fbf84c9462c226f16c56ab36f9b74f167
gabriellongshaw <gabriel.longshaw@gmail.com>
2026-04-29 20:46:44 +0100

    fix (assets/js/data/pages.js): fix broken page urls

--- assets/js/data/pages.js
+++ assets/js/data/pages.js
@@ -1,6 +1,6 @@
 export const pages = [
-  { name: 'Home', href: '/' },
-  { name: 'Aperture', href: '/aperture/' },
-  { name: 'Shutter Speed', href: '/shutter-speed/' },
-  { name: 'ISO', href: '/iso/' },
+  { name: 'Home', href: './' },
+  { name: 'Aperture', href: 'aperture/' },
+  { name: 'Shutter Speed', href: 'shutter-speed/' },
+  { name: 'ISO', href: 'iso/' },
 ];
